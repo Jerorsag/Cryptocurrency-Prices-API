3740cb1d36f5a238ad5df74aa4dc88d1ddc7c594
Jeronimo Rodriguez <jeronimoroseag@gmail.com>
2024-11-16 17:01:58 -0500

    Form Commit

---
+++ js/app.js
@@ -0,0 +1,75 @@
+const criptomonedasSelect = document.querySelector('#criptomonedas');
+const monedaSelect = document.querySelector('#moneda');
+const formulario = document.querySelector('#formulario');
+
+const objBusqueda = {
+    moneda: '',
+    criptomoneda: ''
+}
+
+// Crear un Promise 
+const obtenerCriptomonedas = criptomonedas => new Promise( resolve => {
+    resolve(criptomonedas);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    consultarCriptomonedas();
+
+    formulario.addEventListener('submit', submitFormulario)
+
+    criptomonedasSelect.addEventListener('change', leerValor);
+    monedaSelect.addEventListener('change', leerValor);
+});
+
+function consultarCriptomonedas() {
+    const url = 'https://min-api.cryptocompare.com/data/top/totaltoptiervolfull?limit=20&tsym=USD';
+
+    fetch(url)
+        .then( respuesta => respuesta.json() )
+        .then( resultado => obtenerCriptomonedas(resultado.Data))
+        .then( criptomonedas => selectCriptomonedas(criptomonedas));
+}
+
+function selectCriptomonedas(criptomonedas) {
+    criptomonedas.forEach( cripto => {
+        const { FullName, Name } = cripto.CoinInfo;
+
+        const option = document.createElement('OPTION');
+        option.value = Name;
+        option.textContent = FullName;
+        criptomonedasSelect.appendChild(option); 
+    })
+}
+
+function leerValor(e) {
+    objBusqueda[e.target.name] = e.target.value;
+}
+
+function submitFormulario(e) {
+    e.preventDefault();
+
+    // Validar
+    const { moneda, criptomoneda } = objBusqueda;
+
+    if(moneda === '' || criptomoneda === '') {
+        mostrarAlerta('Ambos campos son obligatorios');
+        return;
+    }
+}
+
+function mostrarAlerta(msg) {
+    const existeError = document.querySelector('.error');
+
+    if(!existeError) {
+        const divMensaje = document.createElement('DIV');
+        divMensaje.classList.add('error');
+    
+        // Mensaje de error 
+        divMensaje.textContent = msg
+        formulario.appendChild(divMensaje);
+    
+        setTimeout( () => {
+            divMensaje.remove();
+        }, 3000);
+    }
+}
